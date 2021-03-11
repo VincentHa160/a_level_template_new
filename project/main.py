f1259d66a60b8dfac2a5df1fd0c81145bae34a13
VincentHa160 <80474996+VincentHa160@users.noreply.github.com>
2021-03-11 16:40:56 +0000

    Update main.py
    a

--- project/main.py
+++ project/main.py
@@ -1,7 +1,8 @@
 # Trial function
 def greeting(name):
     return f'Hello {name}!'
-
+def hello:
+    print("hello")
 
 # Run this script when this module is run directly
 if __name__ == "__main__":
